b5d274833b62d001df1d4081f24dae506a12004b
José Luis Quispe Luyo <jose.quispe@vallegrande.edu.pe>
2020-03-25 03:18:04 -0500

    first commit

---
+++ script/main.js
@@ -0,0 +1,13 @@
+console.log("main.js")
+
+function cambiar_fondo_red() {
+    document.body.style.backgroundColor = "red";
+}
+
+function cambiar_fondo_yellow() {
+    document.body.style.backgroundColor = "yellow";
+}
+
+function cambiar_fondo_green() {
+    document.body.style.backgroundColor = "green";
+}
